df836036b54523364c31e68c331580689e5a3dec
CaioNunes <caiofelipe147@gmail.com>
2018-09-03 18:28:47 -0300

    Simple fix in 'Turn' method

--- src/Poker.hs
+++ src/Poker.hs
@@ -31,29 +31,27 @@ development = do
   clear
   putStrLn "Essa feature está sendo desenvolvida. Por favor aguarde novas atualizações do sistema";
 
-showActions :: [IO] -> IO()
-showActions xs = do
-  if ((head xs) == "1") then (putStrLn bet) 
+-- showActions :: [IO] -> IO()
+-- showActions xs = do
+--   if ((head xs) == "1") then (putStrLn bet)
 
+-- opcao bot1, opcao bot2, opcao bot3, opcao bot 4, opcao bot 5, valorApostaAtual,
 
-
--- opcao bot1, opcao bot2, opcao bot3, opcao bot 4, opcao bot 5, valorApostaAtual, 
-turn :: Int > Int -> Int -> Int -> Int -> IO()  	
-turn op1 op2 op3 op4 op5 valorAposta = do
+-- valor da aposta ( Começa com o valor inicial )
+turn :: Int -> IO()
+turn betValue = do
   clear
   putStrLn "=== SEU TURNO ===\n";
   putStrLn "Valor Aposta: "
-  putStrLn valorAposta
+  putStrLn betValue
   putStrLn "Escolha uma ação\n";
   putStrLn "1 - Apostar\n";
   putStrLn "2 - Cobrir apostar\n";
   putStrLn "3 - Fugir\n";
   putStrLn "4 - Passar\n";
-  option <- getLine; 
+  option <- getLine;
   case option of
     "1" -> development
     "2" -> development
     "3" -> development
     "4" -> development
-
-
