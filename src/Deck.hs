9829a8bf2aa26280c7daa1b2e21343a73c3b576b
rodrigocam <rodrigo.redcode@gmail.com>
2018-09-04 00:27:38 -0300

    Refactor hand functions

--- src/Deck.hs
+++ src/Deck.hs
@@ -1,16 +1,14 @@
 module Deck (
     Card(..),
     Hand(..),
+    ranks,
     deck,
     pick,
     giveCards,
     remainingDeck,
     remainingCards,
-    splitHands,
-    splitHand,
-    createHand,
-    canMakeHand,
-    ranks,
+    splitCardTuples,
+    splitCardTuple,
 ) where
 
 import System.Random
@@ -50,21 +48,9 @@ pick xs = do
     return (xs !! i)
 
 
-hand :: [Card] -> Int -> IO [Hand]
-hand xs n = 
-    core xs n [] []
-    where core xs n c_acc h_acc = do
-            c <- pick xs
-            if n == 0
-                then return h_acc
-            else if canMakeHand c_acc
-                then core (remaining xs c_acc c) (n-1) [] (createHand c_acc:h_acc)
-            else
-                core (remaining xs c_acc c) n (c:c_acc) h_acc
-
-          remaining xs ys e = [x | x <- xs, x /= e, e `notElem` ys]
-
-
+-- Give cards for n players --
+--      >> xs - a list of available cards
+--      >> n - the number of players
 giveCards :: [Card] -> Int -> IO [(Card, Card)]
 giveCards xs n =
     core xs (n*2) []
@@ -77,7 +63,6 @@ giveCards xs n =
 
           remaining xs ys e = [x | x <- xs, x /= e, e `notElem` ys]
           
-
 
 -- Return a deck with all remaining cards on a deck --
 --      >> xs - a list with already used cards
@@ -93,28 +78,14 @@ remainingDeck xs = do
 remainingCards xs ys = [x | x <- xs, x `notElem` ys]
 
 
--- Convert a list of hands of cards to a list of cards --
---      >> xs - a list of hands
-splitHands :: IO [Hand] -> IO [Card]
-splitHands xs = do
+-- Convert a list of card tuples to a list of cards --
+--      >> xs - a list of card tuples
+splitCardTuples :: IO [(Card, Card)] -> IO [Card]
+splitCardTuples xs = do
     ys <- xs
-    return $ concat [splitHand x | x <- ys]
+    return $ concat [splitCardTuple x | x <- ys]
 
 
--- Convert one unique hand into a list of cards --
-splitHand :: Hand -> [Card]
-splitHand (Hand c1 c2) = [c1, c2]
-
-
--- Create a hand with the two first elements on a list --
-createHand :: [Card] -> Hand
-createHand [] = error "You must provide at least two cards for making a hand"
-createHand [n] = error "You must provide at least two cards for making a hand"
-createHand x = Hand (head x) (head (tail x))
-
-
--- Verify if a list of cards can generate a hand --
-canMakeHand :: [Card] -> Bool
-canMakeHand xs
-    | null xs = False
-    | otherwise = length xs >= 2
+-- Convert one unique tuple of cards into a list of cards --
+splitCardTuple :: (Card, Card) -> [Card]
+splitCardTuple (a, b) = [a, b]
