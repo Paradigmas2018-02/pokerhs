b700dfd45c1286da12e5ee0f30c3c992a00a5b8a
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-07 12:31:08 -0300

    Create finder methods for cards

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,9 +1,16 @@
 module Utils(
     createTuples,
     destroyTuples,
-    development
+    development,
+    findCard
 ) where
 
+import Deck(Rank(..), Card(..), Suit(..))
+
+-- Convert a list of card tuples to a list of cards --
+--      >> xs - a list of card tuples
+splitCardTuples :: IO [(Card, Card)] -> IO [Card]
+splitCardTuples xs = destroyTuples <$> xs
 
 -- Create a list of tuples with the given list --
 -- by combining two elements at time, the list must have
@@ -27,3 +34,26 @@ destroyTuple (x, y) = [x, y]
 
 development :: IO()
 development = putStrLn "Essa feature está sendo desenvolvida. Por favor aguarde novas atualizações do sistema";
+
+-- Find a card with the same value on the list
+findByValue :: [Card] -> Card -> Maybe Card
+findByValue list card 
+    | null list = Nothing
+    | rvalue (rank (head list)) == rvalue (rank card) = Just (head list)
+    | otherwise = findByValue (tail list) card
+
+
+-- Find a card with the same suit on the list
+findBySuit :: [Card] -> Card -> Maybe Card
+findBySuit list card 
+    | null list = Nothing
+    | svalue (suit (head list)) == svalue (suit card) = Just (head list)
+    | otherwise = findBySuit (tail list) card
+
+
+-- Find a card with the same value and suit on the list
+findCard :: [Card] -> Card -> Maybe Card
+findCard list card 
+    | null list = Nothing
+    | (svalue (suit (head list)) == svalue (suit card)) && (rvalue (rank (head list)) == rvalue (rank card))  = Just (head list)
+    | otherwise = findCard (tail list) card
