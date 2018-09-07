2442dcd1bcf547ae3064eba3c185d15fba13352a
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-07 16:03:22 -0300

    Create some hand value constants

--- src/Find.hs
+++ src/Find.hs
@@ -7,13 +7,24 @@ import Utils (findByValue)
 import Deck (Card, remainingCards)
 import Poker (Hand(..))
 
+valueHighCard = 1
+valuePair = 2
+valueTwoPair = 3
+valueThreeKind = 4
+valueStraight = 5
+valueFlush = 6
+valueFullHouse = 7
+valueFourKind = 8
+valueStraightFlush = 9
+valueRoyalFlush = 10
+
 findPair :: [Card] -> Maybe Hand
 findPair list
     | null list = Nothing
     | length list == 1 = Nothing
     | otherwise = 
         case findByValue (tail list) (head list) of
-            Just card -> Just (Hand {hvalue=2, hcards=[head list, card]}) 
+            Just card -> Just (Hand {hvalue=valuePair, hcards=[head list, card]}) 
             Nothing -> findPair (tail list)
 
             
@@ -22,7 +33,7 @@ findPair list
 findThreeKind :: [Card] -> Maybe Hand
 findThreeKind xs =
     let core xs acc 
-            | length acc == 3 = Just (Hand {hvalue = 4, hcards = acc})
+            | length acc == 3 = Just (Hand {hvalue = valueThreeKind, hcards = acc})
             | null xs = Nothing
             | null acc =
                 case findPair xs of 
@@ -41,7 +52,7 @@ findThreeKind xs =
 findTwoPairs :: [Card] -> Maybe Hand
 findTwoPairs xs =
     let core xs acc
-            | length acc == 4 = Just (Hand {hvalue = 3, hcards = acc})
+            | length acc == 4 = Just (Hand {hvalue = valueTwoPair, hcards = acc})
             | null xs = Nothing
             | null acc =
                 case findPair xs of 
@@ -59,7 +70,7 @@ findTwoPairs xs =
 findFullHouse :: [Card] -> Maybe Hand
 findFullHouse xs =
     let core xs acc
-            | length acc == 5 = Just (Hand {hvalue = 7, hcards = acc}) 
+            | length acc == 5 = Just (Hand {hvalue = valueFullHouse, hcards = acc}) 
             | null xs = Nothing
             | null acc =
                 -- Three of a kind must be found first or it will be matched as a pair.
@@ -70,6 +81,6 @@ findFullHouse xs =
                 case findPair xs of
                     Just hand -> core xs (hcards hand ++ acc)
                     Nothing -> Nothing
-                    
+
     -- Less than 4 cards can't be a Full House
     in if length xs <= 4 then Nothing else core xs []
