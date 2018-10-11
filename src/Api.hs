b89d1534cb2d31914312aa16722428c4a9177118
rodrigocam <rodrigo.redcode@gmail.com>
2018-10-11 11:07:41 -0300

    Refactor bet route

--- src/Api.hs
+++ src/Api.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
 import GHC.Generics
-import Data.Aeson (ToJSON)
+import Data.Aeson (ToJSON, FromJSON)
 import Web.Scotty
 import Control.Monad.IO.Class
 import Control.Concurrent.MVar
-import Poker(flop, giveCards, Table(..))
+import Poker(flop, giveCards, Table(..), Hand(..))
 import Deck(Card(..), Rank(..), deck, remainingDeck, pick)
 import Player(Player(..), bet)
 import Utils(destroyTuples, destroyTuple)
@@ -14,6 +14,8 @@ import Find(findHand)
 data Game = Game {player1 :: Player, player2 :: Player, table :: Table} deriving(Generic, Show)
 instance ToJSON Game
 
+data Bet = Bet {player :: String, value :: Int} deriving(Generic, Show)
+instance FromJSON Bet
 
 main = do
     mgame <- newGame
@@ -21,6 +23,10 @@ main = do
 
     scotty 3000 $ do
         get "/deck" $ json deck
+
+        get "/winner" $ do
+            g <- liftIO $ readMVar game
+            json $ winner g
 
         get "/newgame" $ do
             g <- liftIO newGame
@@ -32,25 +38,41 @@ main = do
             c <- liftIO $ pick $ remainingDeck (tcards (table g) ++ destroyTuple (cards $ player1 g) ++ destroyTuple (cards $ player2 g))
             liftIO $ modifyMVar game $ \game' ->
                 return (Game {player1= player1 g, player2 = player2 g, table = Table {tcards = c : tcards (table g), pot = pot $ table g, thand = findHand $ c : tcards (table g)}}, True)
-            json c
+            ng <- liftIO $ readMVar game
+            json ng
            
         get "/game" $ do
             g <- liftIO $ readMVar game
             json g
-    
-        get "/human/bet" $ do
-            value <- param "value"
+            
+        post "/bet" $ do
+            b <- jsonData
             g <- liftIO $ readMVar game
-            liftIO $ modifyMVar game $ \game' ->
-                return (Game {player1= fst (bet (player1 g) value (pot $ table g)), player2 = player2 g, table = Table {tcards = tcards $ table g, pot = snd ( bet (player1 g) value (pot $ table g) ), thand = findHand $ tcards (table g)}}, True)
-            ng <- liftIO $ readMVar game
-            json ng
-        
-        get "/bot/bet" $ do
-            value <- param "value"
-            g <- liftIO $ readMVar game
-            liftIO $ modifyMVar game $ \game' ->
-                return (Game {player1= player1 g, player2 = fst (bet (player2 g) value (pot $ table g)), table = Table {tcards = tcards $ table g, pot = snd ( bet (player2 g) value (pot $ table g) ), thand = findHand $ tcards (table g)}}, True)
+            
+            case player b of
+                "player1" -> 
+                    liftIO $ modifyMVar game $ \game' ->
+                        return (Game {
+                            player1 = fst (bet (player1 g) (value b) (pot $ table g)),
+                            player2 = player2 g, 
+                            table = Table {
+                                tcards = tcards $ table g,
+                                pot = snd (bet (player1 g) (value b) (pot $ table g)),
+                                thand = thand $ table g
+                                }
+                        }, True)
+                "player2" ->
+                    liftIO $ modifyMVar game $ \game' ->
+                        return (Game {
+                            player1 = player1 g,
+                            player2 = fst (bet (player2 g) (value b) (pot $ table g)),
+                            table = Table {
+                                tcards = tcards $ table g,
+                                pot = snd (bet (player2 g) (value b) (pot $ table g)),
+                                thand = findHand $ tcards (table g)
+                                }
+                        }, True)
+
             ng <- liftIO $ readMVar game
             json ng
 
@@ -65,3 +87,17 @@ newGame = do
             player2 = Player {name="Human", tokens=200, cards=xs !! 1, phand=findHand $ f ++ destroyTuple (xs !! 1), payment = 0},
             table = Table {tcards = f, pot = 0, thand = findHand f}
         }
+
+
+winner :: Game -> Player
+winner g =
+    if (hvalue $ findHand (aux (player1 g) ++ tcards (table g))) >
+        (hvalue $ findHand (aux (player2 g) ++ tcards (table g)))
+        then player1 g
+    else
+        player2 g
+
+
+aux :: Player -> [Card]
+aux p =
+    destroyTuple (cards p)
